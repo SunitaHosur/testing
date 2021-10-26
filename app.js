69b10f6d891a60eb282d201accc058b7fc3525dd
Sunita Hosur <75924644+SunitaHosur@users.noreply.github.com>
2021-10-26 03:46:26 -0400

    appcode

---
+++ app.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/std", {useNewUrlParser:true, useUnifiedTopology: true}).then(()=>console.log('connect successfully')).catch((err)=>console.log(err));
+mongoose.Schema
